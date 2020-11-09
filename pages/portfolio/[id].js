6b62bc026f3be597cc60d8a5efddea40963fb519
Jossic <jossic.lapierre@gmail.com>
2020-11-09 14:30:54 +0100

    next-portfolio - 62 - Get post by ID

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react'
 import BaseLayout from '../../components/layouts/BaseLayout'
 import axios from 'axios';
-import Link from 'next/Link'
 
 import { withRouter } from 'next/router'
 
 class Portfolio extends Component {
 
+    static async getInitialProps({ query }) {
+        let post = {}
+        try {
+            const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${query.id}`)
+            post = res.data
+        }
+        catch (error) {
+            console.log(error);
+        }
+        return { post }
+    }
+
     render() {
+        const { post } = this.props
         return (
             <BaseLayout>
-                <h1>Page Portfolio</h1>
-                <h2>{this.props.router.query.id}</h2>
+                <h1>{post.title}</h1>
+                <p>{post.id}</p>
+                <p>{post.body}</p>
             </BaseLayout>
         )
     }
