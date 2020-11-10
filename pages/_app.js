c4619aa36791ab9b1e88bf984d6d6b53bc00d167
Jossic <jossic.lapierre@gmail.com>
2020-11-10 13:58:51 +0100

    next-portfolio - 68 - Cleanup + Debugging

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/globals.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import React, { Component } from 'react'
 
-export default MyApp
+export default class App extends Component {
+
+  static async getInitialProps({ Component, context }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(context)
+    }
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+    return <Component {...pageProps} />
+
+  }
+}
